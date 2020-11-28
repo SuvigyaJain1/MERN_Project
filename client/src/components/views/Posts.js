2f1168c96ac864af3ae26ba267ce7110e7e2a06d
Suvigya <suvigyajain1@gmail.com>
2020-11-28 07:54:19 +0530

    Empty page message fixed

--- client/src/components/views/Posts.js
+++ client/src/components/views/Posts.js
@@ -4,8 +4,11 @@ import PostCard from './PostCard';
 export default function Posts(props){
   const posts = props.posts.map((post) => {return <PostCard post={post} />})
   return(
-      <div style={{'margin':'10px'}}>
-        { posts.length != 0 ? posts : <h1> Looks like there are no posts... </h1>}
+      <div style={{'margin':'20px'}}>
+        { posts.length !== 0 ? posts : <h1 style={{
+          "margin-top":"69px",
+          "font-family":"Roboto"
+        }}> Looks like there are no posts... </h1>}
       </div>
   )
 }
